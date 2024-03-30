bebc70ba356549b7dc1b98d771c7ab60e634c477
utyfull <129620067+utyfull@users.noreply.github.com>
2024-03-31 02:13:50 +0300

    Revert "Revert "Db shema (#8)""
    This reverts commit c977a559add6ac9f3677260acaef364f36ca05dd.

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from .models import Book
 
+
+def home_page(request):
+    # получаем все значения модели
+    data = Book.objects.all()
+    return render(request, 'home_page.html', {'data': data})
 # Create your views here.
